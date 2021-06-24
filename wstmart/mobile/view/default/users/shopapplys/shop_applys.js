18cf2e01d1a6bcc04349e7bd023081190b3e7e51
Anlong zhao <>
2021-06-24 17:26:16 +0800

    no message

--- wstmart/mobile/view/default/users/shopapplys/shop_applys.js
+++ wstmart/mobile/view/default/users/shopapplys/shop_applys.js
@@ -6,6 +6,8 @@ function save(){
     WST.msg('请填写联系方式','info');
     return;
   }
+  var cardNumber = $.trim($('#cardNumber').val());
+
   var linkman = $.trim($('#linkman').val());
   if(linkman==''){
     WST.msg('请填写联系人','info');
@@ -19,6 +21,7 @@ function save(){
   var param = {};
   param.linkman = linkman;
   param.linkPhone = linkPhone;
+  param.cardNumber = cardNumber;
   param.applyIntention = applyIntention;
   $.post(WST.U('mobile/shopapplys/add'),param,function(data){
     var json = WST.toJson(data);

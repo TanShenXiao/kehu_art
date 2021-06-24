91d57c0eb889d29632484a86ceda3c69cee2a330
Anlong zhao <>
2021-06-24 17:18:09 +0800

    no message

--- wstmart/wechat/controller/Shopapplys.php
+++ wstmart/wechat/controller/Shopapplys.php
@@ -29,9 +29,10 @@ class Shopapplys extends Base{
         $userId = (int)session('WST_USER.userId');
         // 获取是否已经填写商家入驻
         $isApply = $m->isApply();
-        $rs = $um->getFieldsById($userId,'userPhone');
+        $rs = $um->getFieldsById($userId,'userPhone,cardNumber');
         $this->assign('isApply',$isApply);
         $this->assign('userPhone',$rs['userPhone']);
+        $this->assign('cardNumber',$rs['cardNumber']);
         return $this->fetch('users/shopapplys/shop_applys');
     }
 

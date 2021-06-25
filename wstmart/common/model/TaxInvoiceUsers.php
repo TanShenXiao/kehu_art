de67c6338db0fd69883f9ad414760598e3d2effd
Anlong zhao <>
2021-06-25 10:59:26 +0800

    no message

--- wstmart/common/model/TaxInvoiceUsers.php
+++ wstmart/common/model/TaxInvoiceUsers.php
@@ -185,7 +185,7 @@ class TaxInvoiceUsers extends Base{
     	//AES, 128 模式加密数据 CBC
     	$screct_key = base64_decode($screct_key);
     	$str = trim($str);
-    	$str = addPKCS7Padding($str);
+    	$str = $this->addPKCS7Padding($str);
     	$iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128,MCRYPT_MODE_CBC),1);
     	$encrypt_str =  mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $screct_key, $str, MCRYPT_MODE_CBC);
     	return base64_encode($encrypt_str);

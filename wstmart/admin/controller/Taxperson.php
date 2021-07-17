6bd47cf275d669810d4eb37817e04e2da185e55a
Anlong zhao <>
2021-07-17 23:26:00 +0800

    no message

---
+++ wstmart/admin/controller/Taxperson.php
@@ -0,0 +1,33 @@
+<?php
+namespace wstmart\admin\controller;
+use wstmart\common\model\Taxperson as M;
+
+class Taxperson extends Base{
+
+    /**
+     * 开票人员列表 s
+     * @return mixed|string
+     */
+    public function index()
+    {
+
+        $this->assign("userId",(int)input('userId'));
+        $this->assign("p",(int)input("p"));
+
+    	return $this->fetch("list");
+    }
+
+    /**
+     * 获取分页
+     */
+    public function pageQuery(){
+        $m = new M();
+        $type = input('type',1);
+
+        if($type == 1){
+            return WSTGrid($m->pageQuery());
+        }else{
+            return WSTGrid($m->pageManualQuery());
+        }
+    }
+}

67b7980c6c7e870b14e3d9f79fce87c13c5d5022
Anlong zhao <>
2021-07-17 19:52:50 +0800

    no message

--- wstmart/common/model/Carts.php
+++ wstmart/common/model/Carts.php
@@ -194,7 +194,7 @@ class Carts extends Base{
 		           ->join($prefix.'shops','s.shopId=g.shopId','left')
 		           ->join($prefix.'goods_specs','c.goodsSpecId=gs.id','left')
 		           ->where($where)
-		           ->field('c.goodsSpecId,c.cartId,s.userId,s.shopId,s.shopName,s.shopImg,s.isTaxAuth,g.goodsId,g.marketPrice,g.shippingFeeType,g.shopExpressId,s.shopQQ,shopWangWang,g.goodsName,g.shopPrice,g.shopPrice defaultShopPrice,g.goodsStock,g.goodsWeight,g.goodsVolume,g.isSpec,gs.specPrice,gs.specStock,gs.specWeight,gs.specVolume,g.goodsImg,c.isCheck,gs.specIds,c.cartNum,g.goodsCatId,g.isFreeShipping,g.goodsType,g.saleType,g.oriGoodsId')
+		           ->field('c.goodsSpecId,c.cartId,s.userId,s.shopId,s.shopName,s.shopImg,s.shopType,s.isTaxAuth,g.goodsId,g.marketPrice,g.shippingFeeType,g.shopExpressId,s.shopQQ,shopWangWang,g.goodsName,g.shopPrice,g.shopPrice defaultShopPrice,g.goodsStock,g.goodsWeight,g.goodsVolume,g.isSpec,gs.specPrice,gs.specStock,gs.specWeight,gs.specVolume,g.goodsImg,c.isCheck,gs.specIds,c.cartNum,g.goodsCatId,g.isFreeShipping,g.goodsType,g.saleType,g.oriGoodsId')
 		           ->select();		
 		$carts = [];
 		$goodsIds = [];
